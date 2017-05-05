251580d6e4bceb27055923ca651bb4c7d42e326d
CyrilHskt <c.hasquette@gmail.com>
2017-05-05 15:02:27 +0200

    Creating form and state

--- markdown/src/index.js
+++ markdown/src/index.js
@@ -1,2 +1,50 @@
-// Ici on code notre App
-alert('coucou')
+import React from 'react';
+import { render } from 'react-dom';
+//CSS
+import './style/css/bootstrap.min.css';
+import './index.css';
+//JS
+import { sampleText } from './sampleText';
+
+class App extends React.Component {
+    state = {
+        text : sampleText
+    };
+
+    editText = e => {
+        const text = e.target.value;
+        this.setState({text});
+        
+    }
+
+    render (){
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6">
+                        <textarea 
+                            className="form-control" 
+                            name="" 
+                            cols="30" 
+                            rows="35" 
+                            value={this.state.text} 
+                            onChange={e=> this.editText(e)}
+                        >
+                        </textarea>
+                    </div>
+                    <div className="col-sm-6">
+                        <h1>Résultats</h1>
+                        <div>
+                            {this.state.text}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+};
+
+render(
+    <App />,
+    document.getElementById('root')
+);
